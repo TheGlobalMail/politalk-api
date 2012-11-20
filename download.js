9dfa8e73ae685f630bfdc99f0852e1f2594f57dc
B.J. Rossiter <b.j.rossiter@gmail.com>
2012-11-20 15:04:14 +1100

    Download script can now download between specified dates

--- download.js
+++ download.js
@@ -5,17 +5,19 @@ var Hansard = require('./lib/hansard');
 require('date-utils');
 
 var argv = require('optimist')
-  .usage('Usage: $0 -u [url] -d [date]')
+  .usage('Usage: $0 -u [url] -f [from] -t [to]')
   .demand(['u'])
   .alias('u', 'url')
-  .alias('d', 'date')
   .describe('u', 'URL to download OpenAustralia xml files from')
-  .describe('d', 'Date to get hansard scripts after. In format 2012-12-01')
+  .alias('f', 'from')
+  .describe('f', 'Date to get hansard scripts after. In format 2012-12-01')
+  .alias('t', 'to')
+  .describe('t', 'Date to get hansard scripts before. In format 2012-12-01')
   .argv;
 
 function workOutDateToRequest(cb){
-  if (argv.date){
-    cb(null, (new Date(argv.date)).addDays(-1));
+  if (argv.from){
+    cb(null, (new Date(argv.from)).addDays(-1));
   }else{
     Hansard.lastSpeechDate(function(err, date){
       if (!date) cb('No recent date in database. Run with -d instead');
@@ -26,7 +28,8 @@ function workOutDateToRequest(cb){
 
 workOutDateToRequest(function(err, date){
   if (err) return console.error(err.red);
-  var openau = new OpenAu(argv.url, date);
+
+  var openau = new OpenAu(argv.url, date, argv.to);
   var parser = new Hansard.Parser();
 
   openau
